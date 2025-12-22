85e298c0d1a8bc6e255583eaf7939933a9df7ee1
Wasked2202 <99568766+Wasked2202@users.noreply.github.com>
2025-12-22 22:09:32 +0200

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int i, n, x, proizv;
+float num, sum, maxim, maxn;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // Пункт 1
+
+    cout << "ПУНКТ 1\nСколько чисел в последовательности? >> ";
+    cin >> i;
+    n = 1;
+    sum = 0;
+    maxim = -0.36;
+    maxn = 0;
+    for (i; i > 0; i--) {
+        cout << "Введите A" << n << " >> ";
+        cin >> num;
+        if ((num > -0.36) && (num <= 10.1)) {
+            sum += num;
+            if (maxim < num) {
+                maxim = num;
+                maxn = n;
+            }
+        }
+        n++;
+    }
+    if (maxn == 0) {
+        cout << "Подходящих чисел в последовательности найдено не было." << endl;
+    }
+    else {
+        cout << "Сумма: " << sum << "\nНаибольшее: " << maxim << "\nНомер наибольшего в последовательности: " << maxn << endl;
+    }
+    // Пункт 2
+
+    cout << "\nПУНКТ 2\nВведите число x >> ";
+    cin >> x;
+    x = abs(x);
+    proizv = 1;
+    if (x > 1000) {
+        cout << "Некорректный ввод!";
+    }
+    else {
+        do {
+            proizv = proizv * (x % 10);
+            x /= 10;
+        } while (x > 0);
+        cout << "Произведение всех цифр этого числа: " << proizv;
+    }
+
+    return 0;
+}
---
+++ lab5.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <float.h>
+#include <vector>
+using namespace std;
+
+
+int punkt, a, b, c;
+
+int rule(int a, int b) {
+	if (a % b == 0) {
+		return a / b;
+	}
+	if (b % a == 0) {
+		return b / a;
+	}
+	else {
+		return a * b;
+	}
+}
+int rule(int a, int b, int c) {
+	cout << "Исполняется функция работы с тремя числами" << endl;
+	if (a == b && a == c) {
+		return 1;
+	}
+	else {
+		return -1;
+	}
+}
+
+void p1() {
+	int arr[3];
+
+	for (int i = 0; i < 3; ++i) {
+		cin >> arr[i];
+	}
+
+	if (arr[0] != 0 && arr[1] != 0 && arr[2] != 0) {
+		a = arr[0];
+		b = arr[1];
+		c = arr[2];
+		cout << rule(a, b, c);
+	}
+
+	if (arr[0] == 0 && arr[1] != 0 && arr[2] != 0) {
+		a = arr[1];
+		b = arr[2];
+		cout << rule(a, b);
+	}
+	if (arr[0] != 0 && arr[1] == 0 && arr[2] != 0) {
+		a = arr[0];
+		b = arr[2];
+		cout << rule(a, b);
+	}
+	if (arr[0] != 0 && arr[1] != 0 && arr[2] == 0) {
+		a = arr[0];
+		b = arr[1];
+		cout << rule(a, b);
+	}
+
+
+}
+
+void p2() {
+	int sum, raznab, raznba, proizv, sizeflt, sizeint, minint, maxint;
+	float fa, fb, delab, delba, minflt, maxflt;
+
+
+	sum = a + b;
+	raznab = a - b;
+	raznba = b - a;
+	proizv = a * b;
+	fa = a;
+	fb = b;
+	delab = fa / fb;
+	delba = fb / fa;
+	sizeflt = sizeof(float);
+	sizeint = sizeof(int);
+
+	cout << "\nСумма: " << sum << endl;
+	cout << "Разность a-b: " << raznab << endl;
+	cout << "Разность b-a: " << raznba << endl;
+	cout << "Произведение: " << proizv << endl;
+	cout << "Деление a/b: " << delab << endl;
+	cout << "Деление b/a: " << delba << endl;
+
+	minflt = FLT_MIN;
+	maxflt = FLT_MAX;
+	minint = INT_MIN;
+	maxint = INT_MAX;
+
+	cout << "\nТип данных - int, " << sizeint << " байта, или " << sizeint * 8 << " бита (Минимальное значение : " << minint << ", максимальное : " << maxint << endl;
+	cout << "Тип данных - float, " << sizeflt << " байта, или " << sizeflt * 8 << " бита (Минимальное значение : " << minflt << ", максимальное : " << maxflt << endl;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	cin >> punkt;
+
+	if (punkt == 1) {
+		p1();
+	}
+
+	if (punkt == 2) {
+		cout << "Введите первое неотрицательное число >> ";
+		cin >> a;
+		cout << "Введите второе неотрицательное число >> ";
+		cin >> b;
+		p2();
+	}
+
+}
